a9f9f4d4a0867873545cdf29143ecfa35164591c
Denis Pereira <denisprdo@gmail.com>
2023-09-20 20:38:10 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,30 @@
+# Descrição
+
+Este é um programa simples em Python que utiliza a biblioteca NLTK (Natural Language Toolkit).
+
+# Sobre
+
+NLTK é um kit de ferramentas de linguagem natural. É usado para construir programação python. Ajuda trabalhar com dados de linguagens humanas. Oferece uma interface de usuário muito fácil. Ele suporta classificação, vaporização, marcação, etc.
+
+## Pré-requisitos
+
+Antes de executar o programa, certifique-se de que você tenha Python instalado em seu sistema. Você também precisará instalar a biblioteca NLTK.
+
+### Instalando o NLTK
+
+Você pode instalar o NLTK usando o gerenciador de pacotes `pip`. Abra o terminal ou prompt de comando e execute o seguinte comando:
+
+```bash
+pip install nltk
+```
+
+
+## Como Usar
+
+1. Clone este repositório ou baixe o arquivo `sample.py`.
+
+2. Execute o programa usando o seguinte comando:
+
+```bash
+python sample.py 'a manhã está bonita'
+```
---
+++ sample.py
@@ -0,0 +1,48 @@
+import nltk,sys
+
+texto = sys.argv[1]
+
+# Acessando o corpus MacMorpho
+mac_morpho = nltk.corpus.mac_morpho
+
+#acessando em blocos de sentenças do MacMorpho
+colection = nltk.corpus.mac_morpho.tagged_sents()
+
+#separamos cada palavra (Tokenização)
+sentense = nltk.word_tokenize(texto)
+
+#Training Unigram Tagger - Determinar uma tag para cada token(palavra)
+unigrama = nltk.tag.UnigramTagger(colection)
+
+#Encontrar os resultados marcados após o treinamento
+array = unigrama.tag(sentense)
+
+#Legendas para tradução dos termos
+arr1 = ['ADJ','ADV-KS','ADV-KS-REL','ART','KC','KS','IN','N','NPROP','NUM','PCP','PDEN','PREP','PROADJ','PRO-KS','PROPESS','PRO-KS-REL','PROSUB','V','VAUX','CUR']
+arr2 = [
+    'Adjetivo',
+    'Advérbio conectivo subordinativo',
+    'Advérbio relativo subordinativo',
+    'Artigo def. ou indef.',
+    'Conjunção coordenativa',
+    'Conjunção subordinativa',
+    'Interjeição',
+    'Substantivo',
+    'Substantivo próprio',
+    'Numeral',
+    'Particípio',
+    'Palavra denotativa',
+    'Preposição',
+    'Pronome adjetivo',
+    'Pronome conectivo subordinativo',
+    'Pronome pessoal',
+    'Pronome relativo conectivo subordinativo',
+    'Pronome substantivo',
+    'Verbo',
+    'Verbo auxiliar',
+    'Simbolo de moeda corrente',
+]
+
+#Apresentação dos resultados
+for n in array:
+    print(n[0], ' - ', arr2[arr1.index(n[1])])
